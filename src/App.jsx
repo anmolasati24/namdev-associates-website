4685418b091f9b0719930c6d7ae4ca0af8c8da9b
anmolasati24 <anmolasati24@gmail.com>
2026-03-14 21:46:31 +0530

    fix duplicate imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
 import { Routes, Route } from "react-router-dom";
-import ScrollToTop from "./components/scrolltoTop";
+import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Contact from "./pages/Contact"
-
-import ScrollToTop from "./components/ScrollToTop";
-
+import Contact from "./pages/Contact";
 import ComplianceSection from "./pages/Compliance";
 import ClientsUI from "./pages/Clients";
 import ConsultationPage from "./pages/consultation";
 
-
 function App() {
   return (
     <>
-       <ScrollToTop />
+      <ScrollToTop />
       <Header />
-       <ScrollToTop />
 
       <Routes>
-        
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path ="/compliance" element={<ComplianceSection/>}></Route>
-
-        <Route path="/clients" element={<ClientsUI/>}/>
-        <Route path="/consultation" element={<ConsultationPage/>}/>
-
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/compliance" element={<ComplianceSection />} />
+        <Route path="/clients" element={<ClientsUI />} />
+        <Route path="/consultation" element={<ConsultationPage />} />
       </Routes>
 
       <Footer />
